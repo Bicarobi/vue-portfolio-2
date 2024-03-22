5dbf90119ef8174d9de4793a09788defab527311
Robert C <cmrecki.robert@gmail.com>
2024-03-22 22:13:18 +0100

    Updated about view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -39,18 +39,20 @@ import AboutCard from "../components/AboutCard.vue";
 
 import WebDevIcon from "../components/svgs/WebDevIcon.vue";
 import WebDesignIcon from "../components/svgs/WebDesignIcon.vue";
+import GraphicDesignIcon from "../components/svgs/GraphicDesignIcon.vue";
 import Design3DIcon from "../components/svgs/Design3DIcon.vue";
 import PhotoIcon from "../components/svgs/PhotoIcon.vue";
 
 export default {
 	name: "About",
-	components: { AboutCard, WebDevIcon, WebDesignIcon, Design3DIcon, PhotoIcon },
+	components: { AboutCard, WebDevIcon, WebDesignIcon, GraphicDesignIcon, Design3DIcon, PhotoIcon },
 	computed: {
 		route: () => useRoute(),
 		processedCards() {
 			const cards = [
 				{ type: this.$i18n.t("aboutView.skills.webDev.title"), desc: this.$i18n.t("aboutView.skills.webDev.desc"), image: "WebDevIcon" },
 				{ type: this.$i18n.t("aboutView.skills.webDesign.title"), desc: this.$i18n.t("aboutView.skills.webDesign.desc"), image: "WebDesignIcon" },
+				{ type: this.$i18n.t("aboutView.skills.graphicDesign.title"), desc: this.$i18n.t("aboutView.skills.graphicDesign.desc"), image: "GraphicDesignIcon" },
 				{ type: this.$i18n.t("aboutView.skills.design3D.title"), desc: this.$i18n.t("aboutView.skills.design3D.desc"), image: "Design3DIcon" },
 				{ type: this.$i18n.t("aboutView.skills.photo.title"), desc: this.$i18n.t("aboutView.skills.photo.desc"), image: "PhotoIcon" },
 			];
