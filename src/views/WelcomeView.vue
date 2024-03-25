7e995b8bcd80d4d208039f6affc3e2d09bd9adf4
Robert <cmrecki.robert@gmail.com>
2024-03-25 17:17:14 +0100

    Almost finished styling for mobile

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,3 +1,13 @@
 <template>
-	<h1>{{ $t("welcomeView.text") }}</h1>
+	<div class="welcome-container">
+		<div class="text-container">
+			<div>{{ $t("welcomeView.text1") }}</div>
+			<div class="title">Robert Cmrečki</div>
+			<br />
+			<div>{{ $t("welcomeView.text2") }}</div>
+			<div class="text-more">
+				{{ $t("welcomeView.text3.1") }}<span>{{ $t("welcomeView.text3." + (this.$myGlobalVariable.windowWidth < this.$myGlobalVariable.mobileWindowWidth ? "2" : "3")) }}</span>
+			</div>
+		</div>
+	</div>
 </template>
