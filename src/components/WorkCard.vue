4cf7671790c954f849d70e11654a27a6642608d4
Robert C <cmrecki.robert@gmail.com>
2024-03-26 19:05:57 +0100

    Added modal close icon and arrow icons

--- src/components/WorkCard.vue
+++ src/components/WorkCard.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="work-card-container">
 		<div class="desc-container" @click="showDesc(true, false)" @mouseenter="showDesc(false, true)" @mouseleave="showDesc(false, false)">
-			<img :src="require('../assets/' + img[img.length - 1])" />
-			<div class="desc" :style="this.descHovered ? 'opacity: 1' : 'opacity: 0'">
+			<img :src="require('../assets/' + img[0])" />
+			<div class="desc" :style="this.descClicked || this.descHovered ? 'opacity: 1' : 'opacity: 0'">
 				{{ desc }}
 			</div>
 		</div>
@@ -10,25 +10,32 @@
 			<div class="title">{{ title }}</div>
 			<div class="type">{{ type }}</div>
 		</div>
-		<div class="modal" @click="showDesc(true, false)" :style="this.descClicked ? 'display: fixed' : 'display: none'">
-			<img :src="require('../assets/' + img[img.length - 1])" />
+		<div class="modal" :style="this.descClicked ? 'display: fixed' : 'display: none'">
+			<img :src="require('../assets/' + img[this.index])" />
 			<div class="text-container">
 				<a :href="link"
 					><div class="title">{{ title }}</div></a
 				>
 				<div class="type">{{ desc }}</div>
 			</div>
+			<LeftArrowIcon @click="changeImage(-1)" /><RightArrowIcon @click="changeImage(1)" /><CloseIcon @click="showDesc(true, false)" />
 		</div>
 	</div>
 </template>
 
 <script>
+import LeftArrowIcon from "../components/svgs/LeftArrowIcon.vue";
+import RightArrowIcon from "../components/svgs/RightArrowIcon.vue";
+import CloseIcon from "../components/svgs/CloseIcon.vue";
+
 export default {
 	props: ["title", "type", "desc", "img", "link"],
+	components: { LeftArrowIcon, RightArrowIcon, CloseIcon },
 	data() {
 		return {
 			descClicked: false,
 			descHovered: false,
+			index: 0,
 		};
 	},
 	methods: {
@@ -37,6 +44,15 @@ export default {
 				this.descClicked = !this.descClicked;
 			}
 			this.descHovered = hovered;
+		},
+		changeImage(dir) {
+			this.index += dir;
+
+			if (this.index < 0) {
+				this.index = this.img.length - 1;
+			} else if (this.index > this.img.length - 1) {
+				this.index = 0;
+			}
 		},
 	},
 };
